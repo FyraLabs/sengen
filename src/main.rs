029d71998a30d0d395c1d689786cd07a5d4f628e
Cappy Ishihara <cappy@cappuchino.xyz>
2024-01-08 18:08:20 +0700

    rewrite the funny main db

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
-use surrealdb::{engine::remote::ws::Ws, opt::auth::Root};
-
 mod db;
 mod dbconn;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     dotenvy::dotenv().ok();
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let surrealdb = surrealdb::Surreal::new::<Ws>(&database_url).await?;
+    let surrealdb = &dbconn::get_conn().await?.surrealdb;
 
-    surrealdb
-        .signin(Root {
-            username: "root",
-            password: "root",
-        })
-        .await?;
+
+    let health = surrealdb.health();
+
+    println!("{:?}", health.await?);
 
     Ok(())
 }
